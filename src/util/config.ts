e7635bd5554e9ab9a84aa887aaec25c80fd57045
cjh980402 <9804cjh@naver.com>
2021-12-24 00:07:23 +0900

    Update client version

--- src/util/config.ts
+++ src/util/config.ts
@@ -6,13 +6,13 @@ export class YoutubeConfig {
     static INNERTUBE_API_KEY = 'AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8';
     static INNERTUBE_API_VERSION = 'v1';
     static INNERTUBE_CLIENT_NAME = 'WEB';
-    static INNERTUBE_CLIENT_VERSION = '2.20211216.01.02';
+    static INNERTUBE_CLIENT_VERSION = '2.20211221.00.00';
     static INNERTUBE_CONTEXT = {
         client: {
             hl: 'en',
             gl: 'US',
             clientName: 'WEB',
-            clientVersion: '2.20211216.01.02',
+            clientVersion: '2.20211221.00.00',
             utcOffsetMinutes: 0
         },
         user: {},
