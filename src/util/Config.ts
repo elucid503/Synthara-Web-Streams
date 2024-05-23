e76bf4e6c583635755987fec27f1f33f84d75ee4
= <paulujlaky@gmail.com>
2024-05-23 00:26:48 -0400

    Fix client name being overwritten and causing problems

--- src/util/Config.ts
+++ src/util/Config.ts
@@ -27,8 +27,6 @@ export class YoutubeConfig extends null {
 
             YoutubeConfig.INNERTUBE_API_KEY = json.INNERTUBE_API_KEY;
             YoutubeConfig.INNERTUBE_API_VERSION = json.INNERTUBE_API_VERSION;
-            YoutubeConfig.INNERTUBE_CONTEXT.client.clientName = YoutubeConfig.INNERTUBE_CLIENT_NAME =
-                json.INNERTUBE_CLIENT_NAME;
             YoutubeConfig.INNERTUBE_CONTEXT.client.clientVersion = YoutubeConfig.INNERTUBE_CLIENT_VERSION =
                 json.INNERTUBE_CLIENT_VERSION;
             YoutubeConfig.STS = json.STS;
